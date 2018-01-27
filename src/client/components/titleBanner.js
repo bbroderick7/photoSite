19decaa88d0e1030a809d732edde5a9564f740b3
Brendan Broderick <brendanbroderick95@gmail.com>
2018-01-27 11:41:23 -0600

    try to fix mobile safari bug

--- src/client/components/titleBanner.js
+++ src/client/components/titleBanner.js
@@ -18,21 +18,22 @@ export class TitleBanner extends Component {
   render() {
     let subHeads = "";
     if(this.props.subheaders != undefined){
-      subHeads = this.props.subheaders.map(subhead => <a href={`${this.props.id}/${subhead}`}><h4 className="col-xs-offset-1">{subhead}</h4></a> );
+      subHeads = this.props.subheaders.map(subhead => <a href={ `${this.props.id}/${subhead}` }><h4 className="col-xs-offset-1">{ subhead }</h4></a> );
     }
     let headerDescription = "";
     if(this.props.description != undefined){
-      headerDescription = this.props.description.map(des => <h4 className="col-xs-offset-1">{des}</h4>);
+      headerDescription = this.props.description.map(des => <h4 className="col-xs-offset-1">{ des }</h4>);
     }
     let name = this.props.name == 'portfolio' ? 'stills' : this.props.name
     let new_path = this.props.folder == undefined ? `${name}` : `${this.props.folder}/${name}`;
+    console.log(new_path)
     return(
-      <a href={new_path}>
+      <a href={ new_path }>
         <div id={this.props.name} className="col-lg-4 col-xs-12 landing-divs" onMouseEnter={this.openNav} onMouseLeave={this.closeNav}>
           <h2 className="vertical-heading">{this.props.name}</h2>
             <div id={this.props.nameAlt} className="title-modal">
               <div className="title-modal-content">
-                <h2 className="vertical-heading inner-head">{this.props.name}</h2>
+                <h2 className="vertical-heading inner-head">{ this.props.name }</h2>
                 { subHeads }
                 <h4> { headerDescription } </h4>
               </div>
